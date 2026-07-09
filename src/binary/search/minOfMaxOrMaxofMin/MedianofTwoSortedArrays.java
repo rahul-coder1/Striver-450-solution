d4dc5985676abf5ac310c3a9062e07d212f630c9
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-07-10 00:30:40 +0530

    Leetcode-4: MedianofTwoSortedArrays-better without extra space

--- src/binary/search/minOfMaxOrMaxofMin/MedianofTwoSortedArrays.java
+++ src/binary/search/minOfMaxOrMaxofMin/MedianofTwoSortedArrays.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class MedianofTwoSortedArrays {
 	//Brute force
-	//TC - O(n1+n2)
+	//TC - O(n1+n2), SC - O(n1+n2)
 	public static double bruteFindMedianSortedArrays(int[] nums1, int[] nums2) {
 		int n1=nums1.length, n2=nums2.length;
 		List<Integer> arr = new ArrayList<>();
@@ -22,5 +22,46 @@ public class MedianofTwoSortedArrays {
 		if(N%2==1) return (double) arr.get(N/2);
 		
 		return (double)(arr.get(N/2)+arr.get(N/2-1))/2.0;
+	}
+	
+	//TC - O(n1+n2)
+	public static double betterFindMedianSortedArrays(int[] a, int[] b) {
+		int n1=a.length, n2=b.length;
+		int i=0,j=0,cnt=0;
+		int idx1e=-1,idx2e=-1;
+		int n = n1+n2;
+		int idx2=n/2, idx1=n/2-1; 
+		while(i<n1 && j<n2) {
+			if(a[i]<b[j]) {
+				if(cnt==idx1) idx1e=a[i];
+				if(cnt==idx2) idx2e=a[i];
+				cnt+=1;i+=1;
+			}else {
+				if(cnt==idx1) idx1e=b[j];
+				if(cnt==idx2) idx2e=b[j];
+				cnt+=1;j+=1;
+			}
+		}
+		
+		while(i<n1) {
+			if(cnt==idx1) idx1e=a[i];
+			if(cnt==idx2) idx2e=a[i];
+			cnt+=1;i+=1;
+		}
+		while(j<n2) {
+			if(cnt==idx1) idx1e=b[j];
+			if(cnt==idx2) idx2e=b[j];
+			cnt+=1;j+=1;
+		}
+		
+		if(n%2==1) return (double)idx2e;
+		
+		return (double)(idx2e+idx1e)/2.0;
+	}
+	
+	public static void main(String[] args) {
+//		int[]nums1 = {1,3}, nums2 = {2};
+		int[]nums1 = {1,2}, nums2 = {3,4};
+		System.out.println(betterFindMedianSortedArrays(nums1,nums2));
 	}
 }
