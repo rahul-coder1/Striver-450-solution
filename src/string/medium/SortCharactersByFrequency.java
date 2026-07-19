5c548de0a5933559cb759fec08f50ea51d7c565f
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-07-20 00:43:41 +0530

    Leetcode-451: Sort Characters By Frequency Optimal solution bucket sort

--- src/string/medium/SortCharactersByFrequency.java
+++ src/string/medium/SortCharactersByFrequency.java
@@ -27,10 +27,37 @@ public class SortCharactersByFrequency {
         return res.toString();
     }
     
+    //TC - o(n+k)~n, SC - o(n)
+    public static String frequencySortOptimal(String s) {
+    	List<Character>[] bucket = new ArrayList[s.length()+1];
+    	Map<Character,Integer> mp=new HashMap<>();
+    	StringBuilder res = new StringBuilder();
+    	for(char i: s.toCharArray()) { //****** o(n)
+    		mp.put(i,mp.getOrDefault(i, 0)+1);
+    	}
+    	for(Map.Entry<Character,Integer> entry:mp.entrySet()) {//****** o(k)
+    		int count=entry.getValue(); //count will become the index of bucket
+    		if(bucket[count]==null) bucket[count] = new ArrayList<>();
+    		bucket[count].add(entry.getKey());
+    	}
+    	
+    	for(int i=bucket.length-1;i>=1;i--) { //****** o(n)
+    		if(bucket[i]!=null) {
+    			for(char c : bucket[i]) { 
+    				for(int j=0;j<i;j++) {
+    					res.append(c);
+    				}
+    			}
+    		}
+    	}
+    	
+    	return res.toString();
+    }
+    
     public static void main(String[] args) {
 		String[] arr= {"Aabb","tree","cccaaa"};
 		for(String str:arr)
-			System.out.println(frequencySort(str));
+			System.out.println(frequencySortOptimal(str));
 	}
 }
 
