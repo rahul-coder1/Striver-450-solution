8298f1a67dc41da3dc9bc8a16e95a2d8b82bfdce
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-07-25 02:24:33 +0530

    Leetcode-992: Subarrays with K Different Integers 3 pointer approach

--- src/string/medium/SubarraysWithKDifferentIntegers.java
+++ src/string/medium/SubarraysWithKDifferentIntegers.java
@@ -26,8 +26,10 @@ public class SubarraysWithKDifferentIntegers {
         return calculateDistinctSubarrays(nums,k)-calculateDistinctSubarrays(nums,k-1);
     }
     
+    //----------------------------------------------------------------------------------------------------------------
+    
     //1 <= nums[i], k <= nums.length only then this solution is applicable
-    //TC o()
+    //TC o(2n), SC - o(n)
     public int calculateDistinctSubarraysUsingArray(int[]nums,int k){
     	int l=0,r=0,n=nums.length;
     	int[] mp=new int[n+1];
@@ -47,15 +49,67 @@ public class SubarraysWithKDifferentIntegers {
     	return count;
     }
     
-    public int subarraysWithKDistinctBetter(int[] nums, int k) {
+    public int subarraysWithKDistinctArraySol(int[] nums, int k) {
         return calculateDistinctSubarrays(nums,k)-calculateDistinctSubarrays(nums,k-1);
     }
+    
+    //----------------------------------------------------------------------------------------------------------------
+    
+    public int calculateDistinctSubarrays3Pointer(int[]nums,int k){
+    	int l_far=0,l_near=0,r=0;
+    	int n = nums.length;
+    	int count=0;
+    	Map<Integer,Integer> freq = new HashMap<Integer, Integer>();
+    	while(r<n) {
+    		//adding the element to frequency map
+    		freq.put(nums[r], freq.getOrDefault(nums[r], 0)+1);
+    		
+    		//handles invalid subArray
+    		//handle the case when map size exceeds the k, that is more than k unique element present in map
+    		while(freq.size()>k) {
+    			freq.put(nums[l_near], freq.getOrDefault(nums[l_near], 0)-1);
+    			if(freq.get(nums[l_near])==0)
+    				freq.remove(nums[l_near]);
+    			l_near+=1;
+    			l_far=l_near;
+    		}
+    		
+    		//finds the smallest valid window/subArray
+    		while(freq.get(nums[l_near])>1) {
+    			freq.put(nums[l_near], freq.getOrDefault(nums[l_near], 0)-1);
+    			l_near+=1;
+    		}
+    		
+    		//got the smallest subArray with left far pointer either behind the left near pointer or at same index
+    		if(freq.size()==k) {
+    			count += l_near-l_far+1; //
+    		}
+    		
+    		r+=1;
+    	}
+    	
+    	return count;
+    }
+    
+  //----------------------------------------------------------------------------------------------------------------
+    
+    
     
     public static void main(String[] args) {
     	SubarraysWithKDifferentIntegers call = new SubarraysWithKDifferentIntegers();
     	int[] nums = {1,2,1,2,3}; 
     	int k = 2;
 //    	System.out.println(call.subarraysWithKDistinct(nums, k));
-    	System.out.println(call.subarraysWithKDistinctBetter(nums, k));
+//    	System.out.println(call.subarraysWithKDistinctArraySol(nums, k));
+//    	System.out.println(call.calculateDistinctSubarrays3Pointer(nums, k));
+    	
 	}
 }
+
+
+
+
+
+
+
+
