435711023cabdb72ff03b574a675f2b1205c5ebe
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-07-25 04:23:13 +0530

    Leetcode-5: Longest Palindromic Substring better solution with Expand around center

--- src/string/medium/LongestPalindromicSubstring.java
+++ src/string/medium/LongestPalindromicSubstring.java
@@ -28,11 +28,67 @@ public class LongestPalindromicSubstring {
 	}
 	
 	//---------------------------------------------------------------------------------------------------------
-	
+	//TC - o(n^2), SC - o(1)
+    public static String longestPalindromeExpandAroundCenter(String s) {
+    	int start=0, maxLen=0, n= s.length();
+    	for(int i=0;i<n;i++) {
+    		
+    		//## odd case where both left and right at same index
+    		int left=i,right=i; 
+    		while(left>=0 && right<n && s.charAt(left)==s.charAt(right)) {
+    			if(right-left+1>maxLen) {
+    				maxLen=right-left+1;
+    				start=left;
+    			}
+    			left-=1;right+=1;
+    		}
+    		
+    		//## even case where right is ahead 1 of left at start
+    		left=i;
+    		right=i+1;
+    		while(left>=0 && right<n && s.charAt(left)==s.charAt(right)) {
+    			if(right-left+1>maxLen) {
+    				maxLen=right-left+1;
+    				start=left;
+    			}
+    			left-=1;right+=1;
+    		}
+    	}
+    	
+    	return s.substring(start,start+maxLen);
+    }
 	public static void main(String[] args) {
 		String[] str = {"babad","cbbd"};
 		for(String s:str) {
-			System.out.println(longestPalindromeBrute(s));
+//			System.out.println(longestPalindromeBrute(s));
+			System.out.println(longestPalindromeExpandAroundCenter(s));
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
