ea99e4982c8519069f65c1a4f263c1fe938c4fad
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-08-04 01:49:16 +0530

    Leetcode206. Reverse Linked List via Recursion

--- src/linkedList/medium/ReverseALinkedList.java
+++ src/linkedList/medium/ReverseALinkedList.java
@@ -36,12 +36,24 @@ public class ReverseALinkedList {
 		return prev;
 	}
 	
+	public static ListNode reverseListRecursion(ListNode head) {
+		
+		if(head==null || head.next==null) return head; //base case
+		ListNode newHead = reverseListRecursion(head.next);
+		ListNode front = head.next;
+		front.next = head;
+		head.next=null;
+		
+		return newHead;
+	}
+	
 	public static void main(String[] args) {
 		int[] arr = {10,20,30,40};
 		ListNode head = ListNode.linkedListForming(arr);
 		ListNode.printLL(head);
 //		ListNode.printLL(reverseListBrute(head)); //#1
-		ListNode.printLL(reverseListOptimal(head)); //#1
+//		ListNode.printLL(reverseListOptimal(head)); //#2
+		ListNode.printLL(reverseListRecursion(head)); //#3
 	}
 }
 
