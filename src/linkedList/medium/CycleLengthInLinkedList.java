6da05e12e1a0daa5ca414df6f17b53e3a1d032c6
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-08-06 00:18:33 +0530

    Cycle Length in Linked List Optimal GFG

--- src/linkedList/medium/CycleLengthInLinkedList.java
+++ src/linkedList/medium/CycleLengthInLinkedList.java
@@ -3,6 +3,7 @@ package linkedList.medium;
 import java.util.*;
 
 public class CycleLengthInLinkedList {
+	//TC - o(n*2logn), SC - o(n)
 	public static int lengthOfLoopBrute(ListNode head) {
         Map<ListNode, Integer> mp = new HashMap<>();
         ListNode curr = head;
@@ -19,10 +20,36 @@ public class CycleLengthInLinkedList {
         return 0;
     }
 	
+	//TC - O(x + L) + O(L) ~= 3n ~ o(n)
+	public static int lengthOfLoopOptimal(ListNode head) {
+		ListNode slow = head, fast = head;
+		while(fast!=null && fast.next!=null) {//TC - O(x + L)
+			slow = slow.next;
+			fast = fast.next.next;
+			if(slow==fast) {
+				return cycleLength(slow);
+			}
+		}
+		
+		return 0;
+	}
+	
+	public static int cycleLength(ListNode slow) { //1-2-3-4-5, link=2-5
+		int len=0;
+		ListNode curr = slow;
+		slow = slow.next; len+=1;
+		while(curr!=slow) {//TC - O(L)
+			len+=1;
+			slow = slow.next;
+		}
+			
+		return len;
+	}
+	
 	public static void main(String[] args) {
-		int[] arr = {10,20,30,40};
+		int[] arr = {10,20,30,40,50};
 		ListNode head = ListNode.linkedListForming(arr);
-		head.next.next.next = head.next;
-		System.out.println(lengthOfLoopBrute(head));
+		head.next.next.next.next.next = head.next;
+		System.out.println(lengthOfLoopOptimal(head));
 	}
 }
