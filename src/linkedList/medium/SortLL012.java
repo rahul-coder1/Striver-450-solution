090e768d435323e466fb83991c24100dae50f278
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-08-14 23:04:07 +0530

    Sort a linked list of 0s, 1s and 2s brute force

---
+++ src/linkedList/medium/SortLL012.java
@@ -0,0 +1,40 @@
+package linkedList.medium;
+
+public class SortLL012 {
+	public static ListNode segregateBrute(ListNode head) {
+        if(head==null || head.next==null) return head;
+       int cnt0=0,cnt1=0,cnt2=2;
+       ListNode curr = head;
+       while(curr!=null){
+           if(curr.data==0) cnt0+=1;
+           else if(curr.data==1)cnt1+=1;
+           else if(curr.data==2)cnt2+=1;
+           
+           curr = curr.next;
+       }
+       
+       curr = head;
+       while(curr!=null){
+           if(cnt0>0){
+               curr.data = 0; cnt0-=1;
+           }
+           else if(cnt1>0){
+               curr.data = 1; cnt1-=1;
+           }
+           else if(cnt2>0){
+               curr.data = 2;cnt2-=1;
+           }
+           
+           curr = curr.next;
+       }
+        
+        return head;
+    }
+	
+	public static void main(String[] args) {
+		int[] arr = {2,2,1,1,0,1,0,0};
+		ListNode head = ListNode.linkedListForming(arr);
+		ListNode.printLL(head);
+		ListNode.printLL(segregateBrute(head));
+	}
+}
