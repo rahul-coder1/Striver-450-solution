1f7d44a9d48a83c664dcf34e96f42aff1d3563ff
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-08-15 22:30:25 +0530

    Sort a Linked List of 0's 1's and 2's optimal

--- src/linkedList/medium/SortLL012.java
+++ src/linkedList/medium/SortLL012.java
@@ -1,6 +1,7 @@
 package linkedList.medium;
 
 public class SortLL012 {
+	//TC - o(2n)
 	public static ListNode segregateBrute(ListNode head) {
         if(head==null || head.next==null) return head;
        int cnt0=0,cnt1=0,cnt2=2;
@@ -31,10 +32,42 @@ public class SortLL012 {
         return head;
     }
 	
+	//TC - o(n)
+	public static ListNode segregateOptimal(ListNode head) {
+		if(head==null || head.next==null) return head;
+		ListNode zeroHead = new ListNode(-1);
+		ListNode oneHead = new ListNode(-1);
+		ListNode twoHead = new ListNode(-1);
+		ListNode zero = zeroHead, one = oneHead, two = twoHead;
+		ListNode curr = head;
+        while(curr!=null){
+            if(curr.data==0){
+                zero.next = curr;
+                zero = curr;
+            }
+            else if(curr.data==1){
+                one.next = curr;
+                one = curr;
+            }
+            else{
+                two.next = curr;
+                two = curr;
+            }
+            
+            curr = curr.next;
+        }
+        
+        zero.next = oneHead.next!=null?oneHead.next:twoHead.next;
+        one.next = twoHead.next;
+        two.next = null;
+        
+        return zeroHead.next;
+	}
+	
 	public static void main(String[] args) {
 		int[] arr = {2,2,1,1,0,1,0,0};
 		ListNode head = ListNode.linkedListForming(arr);
 		ListNode.printLL(head);
-		ListNode.printLL(segregateBrute(head));
+		ListNode.printLL(segregateOptimal(head));
 	}
 }
