b3d95e160f9556bf74bd93c14189e371df7fe6b5
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-08-19 01:10:35 +0530

    Add 1 to a Linked List Number brute

---
+++ src/linkedList/medium/Add1ToLL.java
@@ -0,0 +1,54 @@
+package linkedList.medium;
+
+public class Add1ToLL {
+	public static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr!=null){
+            ListNode front = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = front;
+        }
+        return prev;
+    }
+	
+	//TC - o(3n)
+    public static ListNode addOneBrute(ListNode head) {
+        if(head==null) return head;
+        int carry=1;
+        head = reverse(head);
+        ListNode curr = head;
+        while(curr!=null){
+            curr.data = curr.data+carry;
+            if(curr.data<10){
+                carry=0;
+                break;
+            }
+            else{
+                curr.data=0;
+                carry=1;
+            }
+            
+            curr = curr.next;
+        }
+        
+        if(carry==1){
+            ListNode nhead = new ListNode(1);
+            head = reverse(head);
+            nhead.next = head;
+            return nhead;
+        }
+        
+        head = reverse(head);
+        
+        return head;
+    }
+    
+    public static void main(String[] args) {
+		int[] arr = {1,2,3};
+		ListNode head = ListNode.linkedListForming(arr);
+		ListNode.printLL(head);
+		ListNode.printLL(addOneBrute(head));
+	}
+}
