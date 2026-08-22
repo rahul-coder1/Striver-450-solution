5a7eca7cd67ef58a13ae4cbc226f2ac14baf19a8
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-08-22 23:02:59 +0530

    Pair Sum in Sorted Doubly Linked List gfg optimal

--- src/linkedList/doubleLL/medium/PairSumInSortedDoublyLinkedList.java
+++ src/linkedList/doubleLL/medium/PairSumInSortedDoublyLinkedList.java
@@ -3,6 +3,7 @@ package linkedList.doubleLL.medium;
 import java.util.*;
 
 public class PairSumInSortedDoublyLinkedList {
+	//TC - o(n^2)
 	public static List<List<Integer>> givenSumPairsBrute(ListNode head, int target) {
         List<List<Integer>> arr = new ArrayList<>();
         ListNode temp1 = head;
@@ -20,11 +21,40 @@ public class PairSumInSortedDoublyLinkedList {
         return arr;
     }
 	
+	public static ListNode findTail(ListNode head){
+        ListNode temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        
+        return temp;
+    }
+	
+	public static List<List<Integer>> givenSumPairsOptimal(ListNode head, int target) {
+		List<List<Integer>> arr = new ArrayList<>();
+        ListNode left = head;
+        ListNode right = findTail(head);
+        while(left.data<right.data){
+            if(left.data+right.data==target){
+                arr.add(new ArrayList<>(List.of(left.data,right.data)));
+                left = left.next;
+                right = right.prev;
+            }
+            else if(left.data+right.data<target){
+                left = left.next;
+            }else{
+                right = right.prev;
+            }
+        }
+        
+        return arr;
+    }
+	
 	public static void main(String[] args) {
 		int[] arr = {1,2,4,5,6,8,9};
 		ListNode head = ListNode.convertArrToDLL(arr);
 		ListNode.printDLL(head); 
-		List<List<Integer>> a = givenSumPairsBrute(head, 10);
+		List<List<Integer>> a = givenSumPairsOptimal(head, 10);
 		for(List<Integer> i : a) {
 			System.out.println(i);
 		}
