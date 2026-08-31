09678cf1f77003db7ffe7a04506d892ab2d77eaa
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-08-31 23:48:54 +0530

    Leetcode-1922. Count Good Numbers optimal

---
+++ src/recursion/CountGoodNumbers.java
@@ -0,0 +1,32 @@
+package recursion;
+
+public class CountGoodNumbers {
+	//TC - o(logn)
+    public static int countGoodNumbers(long n) {
+        final long MOD = 1_000_000_007;
+        long even = (n+2-1)/2;
+        long odd = n/2;
+
+        return (int)((pow(5,even)*pow(4,odd))%MOD);
+    }
+
+    public static long pow(long x,long n){
+        final long MOD = 1_000_000_007;
+        long res = 1;
+        if(n==0) return 1;
+        while(n>1){
+            if(n%2==1){
+                res = res*x%MOD;
+            }
+            n=n/2;
+            x=(x*x)%MOD;
+        }
+
+        return res*x%MOD;
+    }
+    
+    public static void main(String[] args) {
+		long n = 50;
+		System.out.println(countGoodNumbers(n));
+	}
+}
