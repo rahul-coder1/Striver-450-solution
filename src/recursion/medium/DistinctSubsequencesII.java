c37404fa4ffc24a321cad485f9182d1bb6a78efe
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-09-08 00:42:33 +0530

    Leetcode-940. Distinct Subsequences II bruteforce recursion+HashSet

--- src/recursion/medium/DistinctSubsequencesII.java
+++ src/recursion/medium/DistinctSubsequencesII.java
@@ -1,8 +1,40 @@
 package recursion.medium;
+import java.util.*;
 
 public class DistinctSubsequencesII {
+	//TC - o(n*2^n), SC - o(n*2^n)+o(n - recursion stack)+o(n - stringBuilder) - this sol will 100% give TLE
+	public static int distinctSubseqIIBrute(String s) {
+		Set<String> set = new HashSet<>();
+		StringBuilder sb = new StringBuilder();
+		generate(0,s,set,sb);
+		
+		return set.size();
+	}
+	
+	
+	private static void generate(int idx, String s, Set<String> set, StringBuilder sb) {
+		if(idx>=s.length()) {
+			if(sb.length()!=0) {
+				set.add(sb.toString());
+			}
+			return;
+		}
+		
+		//dont take
+		generate(idx+1, s, set, sb);
+		
+		//take
+		sb.append(s.charAt(idx));
+		generate(idx+1, s, set, sb);
+		
+		//backtrack
+		sb.deleteCharAt(sb.length()-1);
+		
+	}
+
+
 	//TC - o(n), SC - o(26)=o(1)
-    public static int distinctSubseqII(String s) {
+    public static int distinctSubseqIIOptimal(String s) {
         int MOD = 1_000_000_007;
         int total = 0;
         int[] endsWith = new int[26];
@@ -17,8 +49,11 @@ public class DistinctSubsequencesII {
     }
     
     public static void main(String[] args) {
-		System.out.println(distinctSubseqII("abc"));
-		System.out.println(distinctSubseqII("aba"));
-		System.out.println(distinctSubseqII("aaa"));
+		System.out.println(distinctSubseqIIBrute("abc"));
+		System.out.println(distinctSubseqIIBrute("aba"));
+		System.out.println(distinctSubseqIIBrute("aaa"));
+		System.out.println(distinctSubseqIIOptimal("abc"));
+		System.out.println(distinctSubseqIIOptimal("aba"));
+		System.out.println(distinctSubseqIIOptimal("aaa"));
 	}
 }
