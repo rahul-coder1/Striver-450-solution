4bdb9a7147d228eeab075150e54f2997a60e22b5
RK581 <73381901+rahul-coder1@users.noreply.github.com>
2026-09-18 23:34:31 +0530

    Leetcode-216. Combination Sum III optimal

---
+++ src/recursion/medium/CombinationSum3.java
@@ -0,0 +1,45 @@
+package recursion.medium;
+import java.util.*;
+
+public class CombinationSum3 {
+	public static List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> op = new ArrayList<>();
+        generate(1,k,0, n, op, res);
+    
+        return res;
+    }
+
+    static void generate(int i, int k, int sum, int n, List<Integer> op, List<List<Integer>> res){
+        if(sum>n) return;
+        if(k==0){
+            if(sum == n){
+                res.add(new ArrayList<>(op));
+            }
+            return;
+        }
+        if(i>9) return;
+        //dont take
+        generate(i+1,k,sum,n,op,res);
+        //take
+        op.add(i);
+        generate(i+1,k-1,sum+i,n,op,res);
+        op.remove(op.size()-1);
+    }
+    
+    public static void main(String[] args) {
+		System.out.println(combinationSum3(9, 45));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
